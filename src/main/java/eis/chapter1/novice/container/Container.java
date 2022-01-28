51cbf1fff54fda20845e0551f3546015586f049b
Yu-Hang-Z <Yu-Hang.Zhang@outlook.com>
2022-01-28 10:49:59 +0800

    Initial commit

---
+++ src/main/java/eis/chapter1/novice/container/Container.java
@@ -0,0 +1,8 @@
+package eis.chapter1.novice.container;/**
+ * @Classname Container
+ * @Description TODO
+ * @Date 2022/1/27 14:36
+ * @author by zhangyuhang
+ */
+public class Container {
+}
---
+++ src/test/java/eis/chapter1/novice/container/ContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package eis.chapter1.novice.container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
