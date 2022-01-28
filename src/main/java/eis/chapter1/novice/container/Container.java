094d456527c55d3adbbd27b440c207a3686680b7
Yu-Hang-Z <Yu-Hang.Zhang@outlook.com>
2022-01-28 10:52:53 +0800

    创建容器类

--- src/main/java/eis/chapter1/novice/container/Container.java
+++ src/main/java/eis/chapter1/novice/container/Container.java
@@ -1,8 +1,21 @@
-package eis.chapter1.novice.container;/**
+package eis.chapter1.novice.container;
+
+/**
+ * @author by zhangyuhang
  * @Classname Container
  * @Description TODO
  * @Date 2022/1/27 14:36
- * @author by zhangyuhang
  */
 public class Container {
+
+    Container[] g;  // 一组相连的容器
+    int n;          //容器组的实际大小
+    double x;       // 该容器中的水量
+
+    public Container() {
+        g = new Container[1000];  //注意：这是一个魔法数
+        g[0] = this;              //将该容器放入容器组中
+        n = 1;
+        x = 0;
+    }
 }
